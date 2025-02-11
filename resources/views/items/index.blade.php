44480997207929f5b3a7116d5549136609efe034
HondaAkihito <honaki.and.1114@gmail.com>
2025-02-11 20:42:30 +0900

    items.indexのcommentデザイン　修正完了

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -15,13 +15,13 @@
                         <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">検索</button>
                     </form>
                     <div class="lg:w-2/3 w-full mx-auto overflow-auto">
-                        <table class="table-auto w-full text-left whitespace-no-wrap">
+                        <table class="whitespace-nowrap table-auto w-full text-left whitespace-no-wrap">
                         <thead>
                             <tr>
                             <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">詳細</th>
                             <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">食品名</th>
-                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">種類</th>
                             <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">期限</th>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">種類</th>
                             <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">コメント</th>
                             </tr>
                         </thead>
@@ -30,9 +30,9 @@
                             <tr @class(['bg-red-100 text-red-700' => $item->is_near_deadline])>
                             <td class="border-t-2 border-gray-200 px-4 py-3"><a class="text-blue-500" href="{{ route('items.show', ['item' => $item->id]) }}">詳細</a></td>
                             <td class="border-t-2 border-gray-200 px-4 py-3">{{ $item->name }}</td>
-                            <td class="border-t-2 border-gray-200 px-4 py-3">{{ $item->expiration_label }}</td>
                             <td class="border-t-2 border-gray-200 px-4 py-3">{{ $item->deadline }}</td>
-                            <td class="border-t-2 border-gray-200 px-4 py-3">{{ $item->comment }}</td>
+                            <td class="border-t-2 border-gray-200 px-4 py-3">{{ $item->expiration_label }}</td>
+                            <td class="border-t-2 border-gray-200 px-4 py-3 max-w-xs overflow-auto">{{ $item->comment }}</td>
                             </tr>
                             @endforeach
                         </tbody>
