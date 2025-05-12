1069f75192e1bbc91266f2aeaa8edcf51d4d1025
HondaAkihito <honaki.and.1114@gmail.com>
2025-05-12 10:39:49 +0900

    コンポーネントfooter 下部固定 修正

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,4 @@
+<!-- resources/views/components/footer.blade.php -->
+<footer class="bg-white py-4 text-center text-sm text-gray-500 shadow">
+    &copy; {{ date('Y') }} {{ config('app.name') }}. All rights reserved.
+</footer>
