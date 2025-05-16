fa5d53232b0f0895e900b42611a0ab109fd1ced6
HondaAkihito <honaki.and.1114@gmail.com>
2025-05-16 17:08:34 +0900

    <title>修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ファーストビュー</title>
+    <title>{{ config('app.name', '消費期限管理') }}</title>
     {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
